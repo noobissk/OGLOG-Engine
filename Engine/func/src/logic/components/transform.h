71c4f9afbaa50a276814093d76c8c8b3f8b53681
noobissk <noobissk@gmail.com>
2025-10-14 11:19:16 +0200

    more components

--- Engine/func/src/logic/components/transform.h
+++ Engine/func/src/logic/components/transform.h
@@ -15,7 +15,7 @@ struct Transform {
     std::vector<Transform*> children;
 
     inline gml::vec3 euler_angles() {
-        return gml::to_euler(rotation); 
+        return gml::to_euler(rotation);
     }
     inline gml::vec3 local_euler_angles() {
         return gml::to_euler(local_rotation); 
