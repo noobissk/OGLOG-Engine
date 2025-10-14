ff52fdd24705f972d11cb82117703bfda2c41358
noobissk <noobissk@gmail.com>
2025-10-14 10:30:31 +0200

    Transform

---
+++ Engine/func/src/logic/components/transform.h
@@ -0,0 +1,23 @@
+
+#pragma once
+#include <gml/vector.h>
+#include <gml/quaternion.h>
+#include <vector>
+
+struct Transform {
+    gml::vec3 position;
+    gml::vec3 local_pos;
+
+    gml::quat rotation;
+    gml::quat local_rotation;
+
+    Transform* parent;
+    std::vector<Transform*> children;
+
+    inline gml::vec3 euler_angles() {
+        return gml::to_euler(rotation); 
+    }
+    inline gml::vec3 local_euler_angles() {
+        return gml::to_euler(local_rotation); 
+    }
+}
